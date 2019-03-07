3509cbc7360eccabca2c82e688f9c33ed585d2fd
Michael Vreeken <mike@vreeken.dev>
2019-03-06 18:15:20 -0800

    Cornflower+Cream color scheme

--- src/boot/constants.js
+++ src/boot/constants.js
@@ -1,11 +1,11 @@
 export default async({ Vue }) => {
 	const base = 'http://scriptorians.test/';
 	const apiBase = 'http://scriptorians.test/api/';
-	
+
 	Vue.prototype.$urls = {
 		base: base,
 		json: {
-			scriptures: base + base + 'json/scriptures.json',
+			scriptures: base + 'json/scriptures.json',
 			scriptures_summary: base + 'json/scriptures_summary.json'
 		},
 		comments: {
--- src/layouts/MainTemplate.vue
+++ src/layouts/MainTemplate.vue
@@ -1,6 +1,6 @@
 <template>
 	<q-layout view="lHh Lpr lFf">
-		<q-header elevated class="glossy">
+		<q-header elevated class="">
 			<q-toolbar>
 				<q-btn
 					flat
