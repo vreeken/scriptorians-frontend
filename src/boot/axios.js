70581f93911c5ec820582c97894a4ed0f1f9c303
Michael Vreeken <mike@vreeken.dev>
2019-03-12 20:14:03 -0700

    Setup axios to use csrf token received from laravel's blade template

--- src/boot/axios.js
+++ src/boot/axios.js
@@ -2,6 +2,8 @@ import axios from 'axios'
 
 export default async({ Vue }) => {
 	axios.defaults.baseURL = 'http://scriptorians.test/api/';
-	// axios.defaults.withCredentials = true;
+	let token = document.head.querySelector('meta[name="csrf-token"]');
+	axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
+
 	Vue.prototype.$axios = axios
 }
