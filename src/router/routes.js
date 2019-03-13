7b2c84bbfe8291ac47539b1edfe80a15ca484a66
Michael Vreeken <mike@vreeken.dev>
2019-03-12 20:17:01 -0700

    Formatting

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,40 +2,42 @@
 const routes = [{
 	path: '/',
 	component: () => import('layouts/MainTemplate.vue'),
-	children: [{
-		path: '',
-		component: () => import('pages/index.vue')
-	},
-	{
-		path: 'scriptures',
-		component: () => import('layouts/ScripturesTemplate.vue'),
-		children: [
-			{
-				path: '',
-				component: () => import('pages/scriptures/Index.vue')
-			},
-			{
-				path: ':v/',
-				component: () => import('pages/scriptures/VolumeView.vue')
-			},
-			{
-				path: ':v/:b',
-				component: () => import('pages/scriptures/BookView.vue')
-			},
-			{
-				path: ':v/:b/:c',
-				component: () => import('pages/scriptures/ChapterView.vue')
-			}
-		]
-	},
-	{
-		path: 'register',
-		component: () => import('pages/auth/RegisterView.vue')
-	},
-	{
-		path: 'login',
-		component: () => import('pages/auth/LoginView.vue')
-	}]
+	children: [
+		{
+			path: '',
+			component: () => import('pages/index.vue')
+		},
+		{
+			path: 'scriptures',
+			component: () => import('layouts/ScripturesTemplate.vue'),
+			children: [
+				{
+					path: '',
+					component: () => import('pages/scriptures/Index.vue')
+				},
+				{
+					path: ':v/',
+					component: () => import('pages/scriptures/VolumeView.vue')
+				},
+				{
+					path: ':v/:b',
+					component: () => import('pages/scriptures/BookView.vue')
+				},
+				{
+					path: ':v/:b/:c',
+					component: () => import('pages/scriptures/ChapterView.vue')
+				}
+			]
+		},
+		{
+			path: 'register',
+			component: () => import('pages/auth/RegisterView.vue')
+		},
+		{
+			path: 'login',
+			component: () => import('pages/auth/LoginView.vue')
+		}
+	]
 }];
 
 // Always leave this as last one
